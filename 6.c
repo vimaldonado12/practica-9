49dc6810bf587b817af007b77f9984223382058a
vimaldonado12 <vivian.maldonado4y@gmail.com>
2026-04-16 13:49:32 -0600

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,8 @@
+printf("x = *(apArr+1) \t -> x = %d\n", x);
+x = *(apArr+2); /*A la variable x se le asigna el contenido del arreglo arr en su elemento 2*/
+printf("x = *(apArr+2) \t -> x = %d\n", x);
+x = *(apArr+3); /*A la variable x se le asigna el contenido del arreglo arr en su elemento 3*/
+printf("x = *(apArr+3) \t -> x = %d\n", x);
+x = *(apArr+4); /*A la variable x se le asigna el contenido del arreglo arr en su elemento 4*/
+printf("x = *(apArr+2) \t -> x = %d\n", x);
+return 0; }
